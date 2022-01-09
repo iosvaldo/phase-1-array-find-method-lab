ee701b32a111410a3d2e5445c8ee4d12dcf1cbc4
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-08 21:05:04 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function basis(object){
+  return object.result === "W"
+}
+function superbowlWin(array){
+  const data = array.find(basis);
+  if (data){
+      return data.year;
+  }
+}
+console.log(superbowlWin(record));
